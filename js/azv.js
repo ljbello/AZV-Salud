781674f8e9c0c2c1030427c85f0e4cbd0d9401f3
ljbelloxa <31632270+ljbelloxa@users.noreply.github.com>
2018-12-24 13:58:44 -0500

    Cambios
    cambios

--- js/azv.js
+++ js/azv.js
@@ -60,31 +60,31 @@ function shortDate(date) {
 	return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
 }
 
-function getCookie(name) {
-	var cookieValue = null;
-	if (document.cookie && document.cookie !== '') {
-		var cookies = document.cookie.split(';');
-		for (var i = 0; i < cookies.length; i++) {
-			var cookie = jQuery.trim(cookies[i]);
-			// Does this cookie string begin with the name we want?
-			if (cookie.substring(0, name.length + 1) === (name + '=')) {
-				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-				break;
-			}
-		}
-	}
-	return cookieValue;
-}
+// function getCookie(name) {
+// 	var cookieValue = null;
+// 	if (document.cookie && document.cookie !== '') {
+// 		var cookies = document.cookie.split(';');
+// 		for (var i = 0; i < cookies.length; i++) {
+// 			var cookie = jQuery.trim(cookies[i]);
+// 			// Does this cookie string begin with the name we want?
+// 			if (cookie.substring(0, name.length + 1) === (name + '=')) {
+// 				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+// 				break;
+// 			}
+// 		}
+// 	}
+// 	return cookieValue;
+// }
 // var csrftoken = getCookie('csrftoken');
 
-$.ajaxSetup({
-	beforeSend: function (xhr) {
-		// console.log(getCookie('csrftoken'));
-		// console.log(getCookie('ENTRO A LA FUNCION'));
+// $.ajaxSetup({
+// 	beforeSend: function (xhr) {
+// 		// console.log(getCookie('csrftoken'));
+// 		// console.log(getCookie('ENTRO A LA FUNCION'));
 
-		xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken'));
-	}
-});
+// 		xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken'));
+// 	}
+// });
 
 function checkForm() {
 	var isFormValid = true;
