59d58883d8f641095c501c1f75f6230eb06d2de3
ljbelloxa <31632270+ljbelloxa@users.noreply.github.com>
2018-12-27 13:45:26 -0500

    Add Splash
    Add Splash

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	// Calling our splashScreen plugin and
+	// passing an array with images to be shown
+	
+	$('#azv_IMG').splashScreen({
+		textLayers : [
+		]
+	});
+
+});
